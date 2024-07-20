b17558cd38b9855bf0be3c451f9e63dbe4b44e06
VKSridharan <sridharanvk123@gmail.com>
2024-07-20 16:36:00 +0530

    Time: 303 ms (5.02%), Space: 20.8 MB (5.84%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,17 @@
+//Solution 01:
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        vector<int> dp(size(nums), 10001);
+        return help(nums, dp, 0);
+    }
+    int help(vector<int>& nums,  vector<int>& dp, int start){
+        if(start >= size(nums)-1) return 0;
+        if(dp[start] != 10001) return dp[start];
+        
+        for(int i=1; i<=nums[start]; i++){
+            dp[start] = min(dp[start], 1+help(nums, dp, start+i));
+        }
+        return dp[start];
+    } 
+};
